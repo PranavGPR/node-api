b85708f4e8c6dc045be443337d3297db6a44530e
PranavGPR <pranav.gnanasekar23@gmail.com>
2022-01-07 19:47:57 +0530

    content changed

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var http = require('http');
 
 var server = http.createServer(function (req, res) {
     if (req.url == '/') { 
-        res.writeHead(200); 
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>This is admin Page.</p></body></html>');
         res.end();
     } else {
         res.end('Invalid Request!');
